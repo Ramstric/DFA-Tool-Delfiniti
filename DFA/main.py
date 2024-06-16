7259c3ba3d7458151c8992b889c9237eb1677b6d
progr <ramstric@gmail.com>
2024-06-15 20:14:30 -0600

    DFA main setup with random time series

--- DFA/main.py
+++ DFA/main.py
@@ -10,7 +10,6 @@ import matplotlib.animation as animation
 from matplotlib import cm, colors
 import matplotlib as mpl
 
-
 # -----[ Plot style options ]-----
 plt.rcParams['figure.dpi'] = 400
 custom_colors = {"Blue": "#61AFEF", "Orange": "#D49F6E", "Green": "#98C379", "Rose": "#E06C75",
@@ -20,6 +19,21 @@ custom_colors = {"Blue": "#61AFEF", "Orange": "#D49F6E", "Green": "#98C379", "Ro
                  "LightGray": "#CCCCCC", "LightBlack": "#282C34", "Black": "#1D2025"}
 plt.style.use(matplotx.styles.onedark)
 
+# -----[ Time series ]-----
+
+x = torch.rand(100)
+t = torch.linspace(0, 1, x.size()[0])
+
+# -----[ DFA Method ]-----
+
+
+# -----[ Plot ]-----
+plt.plot(t, x, color=custom_colors["Blue"], label="Time series")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.legend()
+plt.ylim(-1, 2)
+plt.show()
 
 def DFA_Method():
     pass
